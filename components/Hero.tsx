d2eea9680176ec518f30913300c80d855cf5f03a
Desmond Awere <desmondawere@gmail.com>
2023-08-02 03:01:20 +0100

    modified Hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,9 +9,9 @@ function Hero() {
     return (
         <div className='hero'>
             <div className='flex-1 pt-36 padding-x'>
-                <h1 className='hero__title'>Find, book, or rent a car -- quickly and easily</h1>
+                <h1 className='hero__title'>Find, car, or swap a car -- quickly and easily</h1>
 
-                <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
+                <p className='hero__subtitle'>Streamline your car swapping experience with our effortless booking process.</p>
 
                 <CustomButton title="Explore Cars" containerStyles="bg-primary-red text-white rounded-full mt-10" handleClick={handleScroll} />
             </div>
